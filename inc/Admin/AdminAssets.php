e8e80d9a42c1e8f1f4bfbcec8b31275fbf80e46b
Parsa Kafi <parselearn@gmail.com>
2025-02-16 03:07:24 +0330

    Add color picker assets to admin

--- inc/Admin/AdminAssets.php
+++ inc/Admin/AdminAssets.php
@@ -5,6 +5,7 @@ namespace WooAssistant\Admin;
 defined( 'ABSPATH' ) || exit;
 
 use WooAssistant\Helper\Assets;
+use WooAssistant\Helper\Param;
 
 class AdminAssets {
 	public function __construct() {
@@ -14,17 +15,22 @@ class AdminAssets {
 	public function enqueueScripts(): void {
 		$pluginVersion = WOOASSISTANT_PLUGIN_VERSION . ( defined( 'DEVELOPMENT_MODE' ) && DEVELOPMENT_MODE ? time() : '' );
 
-		wp_enqueue_style( WOOASSISTANT_PLUGIN_SLUG . '-admin-style',
-			Assets::url( 'css-admin/admin-style.min.css' ), false, $pluginVersion );
+		if ( Param::get( 'page' ) === WOOASSISTANT_PLUGIN_SLUG ) {
+			wp_enqueue_style( 'wp-color-picker' );
+			wp_enqueue_script( 'wp-color-picker' );
 
-		wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-admin',
-			Assets::url( 'js-admin/script.min.js' ),
-			[ 'jquery' ], $pluginVersion, [ 'in_footer' => true ] );
+			wp_enqueue_style( WOOASSISTANT_PLUGIN_SLUG . '-admin-style',
+				Assets::url( 'css-admin/admin-style.min.css' ), false, $pluginVersion );
 
-		wp_localize_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', WOOASSISTANT_PLUGIN_KEYCAP, array(
-			'ajaxurl'   => admin_url( 'admin-ajax.php' ),
-			'ajaxnonce' => wp_create_nonce( WOOASSISTANT_PLUGIN_SLUG . current_time( 'd' ) )
-		) );
+			wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-admin',
+				Assets::url( 'js-admin/script.min.js' ),
+				[ 'jquery' ], $pluginVersion, [ 'in_footer' => true ] );
+
+			wp_localize_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', WOOASSISTANT_PLUGIN_KEYCAP, array(
+				'ajaxurl'   => admin_url( 'admin-ajax.php' ),
+				'ajaxnonce' => wp_create_nonce( WOOASSISTANT_PLUGIN_SLUG . current_time( 'd' ) )
+			) );
+		}
 	}
 
 	public static function imageUrl( $path ): string {
