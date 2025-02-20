ac4d1f0a0996c1dec5e3e790c43a8fc33c7c611d
Parsa Kafi <parselearn@gmail.com>
2025-02-20 22:40:39 +0330

    Add new action for register addon

--- inc/App/App.php
+++ inc/App/App.php
@@ -8,5 +8,13 @@ class App {
 	public function __construct() {
 		new AppAssets();
 		new ProductQuantity();
+		new ProductTest();
+
+		add_action( 'init', [ $this, 'init' ] );
+	}
+
+	public function init() {
+		do_action( 'woo_assistant_plugins_load' );
+		do_action( 'woo_assistant_init' );
 	}
 }
