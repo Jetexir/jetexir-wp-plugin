ab8cb98fbbcf0116d6c852df4e26a3ee1a23dd35
Parsa Kafi <parselearn@gmail.com>
2025-02-20 22:49:53 +0330

    Add Woo Developer feed addon

--- inc/Helper/Helper.php
+++ inc/Helper/Helper.php
@@ -3,6 +3,21 @@
 namespace WooAssistant\Helper;
 
 class Helper {
+	public static function urlToKey( string $url, $hostOnly = false ) {
+		if ( Validating::isUrl( $url ) ) {
+			if ( $hostOnly ) {
+				$url = parse_url( $url, PHP_URL_HOST );
+			} else {
+				$url = parse_url( $url, PHP_URL_HOST ) . parse_url( $url, PHP_URL_PATH );
+				$url = trim( $url, '/' );
+			}
+
+			return Sanitizing::title( $url );
+		}
+
+		return false;
+	}
+
 	public static function combineStyles( $styles ): string {
 		if ( ! is_array( $styles ) || empty( $styles ) ) {
 			return '';
