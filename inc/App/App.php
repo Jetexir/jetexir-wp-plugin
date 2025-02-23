edf89eafe53a2ad0409ee702ca439ef92a6ee16d
Parsa Kafi <parselearn@gmail.com>
2025-02-24 03:14:26 +0330

    Move Product subclass to main Product class

--- inc/App/App.php
+++ inc/App/App.php
@@ -2,13 +2,14 @@
 
 namespace WooAssistant\App;
 
+use WooAssistant\App\Product\Product;
+
 defined( 'ABSPATH' ) || exit;
 
 class App {
 	public function __construct() {
 		new AppAssets();
-		new ProductQuantity();
-		new ProductTest();
+		new Product();
 
 		add_action( 'init', [ $this, 'init' ] );
 	}
@@ -16,5 +17,21 @@ class App {
 	public function init() {
 		do_action( 'woo_assistant_addons_load' );
 		do_action( 'woo_assistant_init' );
+	}
+
+	/**
+	 * Adds a new shortcode.
+	 *
+	 * @param string $tag Shortcode tag to be searched in post content.
+	 * @param callable $callback The callback function to run when the shortcode is found.
+	 *                           Every shortcode callback is passed three parameters by default,
+	 *                           including an array of attributes (`$atts`), the shortcode content
+	 *                           or null if not set (`$content`), and finally the shortcode tag
+	 *                           itself (`$shortcode_tag`), in that order.
+	 */
+	public static function addShortcode( $tag, $callback ) {
+		if ( apply_filters( 'woo_assistant_add_shortcode', true, $tag ) ) {
+			add_shortcode( $tag, $callback );
+		}
 	}
 }
