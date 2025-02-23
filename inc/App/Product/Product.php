95abfdb39ed9be3d99828a1c576c2a6305fcc70f
Parsa Kafi <parselearn@gmail.com>
2025-02-24 03:19:46 +0330

    Move Product class

---
+++ inc/App/Product/Product.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace WooAssistant\App\Product;
+
+class Product {
+	public function __construct() {
+		new ProductQuantity();
+		//new ProductTest();
+		new ProductCompare();
+	}
+
+	/**
+	 * Get product fields
+	 *
+	 * @return array
+	 */
+	public static function getFields(): array {
+		return array(
+			'price'      => __( 'Price', 'woo-assistant' ),
+			'stock'      => __( 'Stock', 'woo-assistant' ),
+			'rating'     => __( 'Rating', 'woo-assistant' ),
+			'brand'      => __( 'Brand', 'woo-assistant' ),
+			'dimensions' => __( 'Dimensions', 'woo-assistant' ),
+			'weight'     => __( 'Weight', 'woo-assistant' ),
+		);
+	}
+
+	public static function getCurrentId(): int {
+		global $product;
+
+		$productID = 0;
+		if ( is_a( $product, 'WC_Product' ) && method_exists( $product, 'get_id' ) ) {
+			$productID = $product->get_id();
+		}
+
+		return $productID;
+	}
+}
