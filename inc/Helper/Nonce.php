57f9f8818e21bce90937390bb95c9fbe24acf034
Parsa Kafi <parselearn@gmail.com>
2025-02-24 03:17:28 +0330

    Add Nonce class

---
+++ inc/Helper/Nonce.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace WooAssistant\Helper;
+
+class Nonce {
+	public static function create( $action = WOOASSISTANT_PLUGIN_KEY ) {
+		return wp_create_nonce( $action );
+	}
+
+	public static function verify( $nonce = null, $action = WOOASSISTANT_PLUGIN_KEY ) {
+		$nonce = is_null( $nonce ) && isset( $_POST['nonce'] ) ? Sanitizing::text( wp_unslash( $_POST['nonce'] ) ) : null;
+		if ( is_null( $nonce ) ) {
+			return false;
+		}
+
+		return wp_verify_nonce( $nonce, $action );
+	}
+}
