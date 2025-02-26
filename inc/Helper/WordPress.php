2fe52ffdeb7d2be78266aefc3a65b713fc17392d
Parsa Kafi <parselearn@gmail.com>
2025-02-26 23:00:19 +0330

    Add blogInfo helper method to WordPress helper class

--- inc/Helper/WordPress.php
+++ inc/Helper/WordPress.php
@@ -3,7 +3,11 @@
 namespace WooAssistant\Helper;
 
 class WordPress {
-	public static function isPage( $page ) {
+	public static function blogInfo( $show = '', $filter = 'raw' ) {
+		return get_bloginfo( $show, $filter );
+	}
+
+	public static function isPage( $page ): bool {
 		return is_page( $page );
 	}
 }
