7cb37ab8d8089edcaed8ec4530649fec495c2563
Parsa Kafi <parselearn@gmail.com>
2025-02-28 04:29:52 +0330

    Add arrayInsertAfter to Helper method

--- inc/Helper/Helper.php
+++ inc/Helper/Helper.php
@@ -3,6 +3,23 @@
 namespace WooAssistant\Helper;
 
 class Helper {
+	/**
+	 * Inserts any number of scalars or arrays at the point
+	 * in the haystack immediately after the search key ($needle) was found,
+	 * or at the end if the needle is not found or not supplied.
+	 * Modifies $haystack in place.
+	 * https://stackoverflow.com/a/7257599/3224296
+	 *
+	 * @param array &$haystack the associative array to search. This will be modified by the function
+	 * @param string $needle the key to search for
+	 * @param array $stuff one or more arrays or scalars to be inserted into $haystack
+	 *
+	 * @return array the index at which $needle was found
+	 */
+	public static function arrayInsertAfter( array $haystack, string $needle = '', array $stuff = [] ) {
+		return array_merge( array_slice( $haystack, 0, $needle, true ), $stuff, array_slice( $haystack, $needle, count( $haystack ) - 1, true ) );
+	}
+
 	public static function urlToKey( string $url, $hostOnly = false ) {
 		if ( Validating::isUrl( $url ) ) {
 			if ( $hostOnly ) {
