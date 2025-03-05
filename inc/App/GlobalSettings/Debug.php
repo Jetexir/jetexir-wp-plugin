d30734614080c88de4115376b776e5720a1c54ce
Parsa Kafi <parselearn@gmail.com>
2025-03-05 21:27:43 +0330

    Add Debug section to Global tab

---
+++ inc/App/GlobalSettings/Debug.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace WooAssistant\App\GlobalSettings;
+
+class Debug {
+	private const sectionID = 'debug';
+
+	public function __construct() {
+		add_filter( 'woo_assistant_global_settings_sections', [ $this, 'addSectionSettings' ] );
+	}
+
+	public function addSectionSettings( array $sections ): array {
+		$settings = [
+			'start_grid_debug_enable' => array(
+				'title' => __( 'Debugging', 'woo-assistant' ),
+				'type'  => 'startGrid',
+			),
+			'debug_enable'            => array(
+				'id'       => 'debug_enable',
+				'title'    => __( 'Enable Debug', 'woo-assistant' ),
+				'type'     => 'toggle',
+				'value'    => 1,
+				'default'  => false,
+				'desc'     => __( 'By enabling this option, the uncompressed version of the JS and CSS files will be loaded.', 'woo-assistant' ),
+				'sanitize' => 'bool'
+			),
+			'end_grid_debug_enable'   => array(
+				'type' => 'endGrid',
+			)
+		];
+
+		$sections[ self::sectionID ] = array(
+			'title'    => __( 'Debug', 'woo-assistant' ),
+			'desc'     => __( 'Debug Settings', 'woo-assistant' ),
+			'settings' => $settings
+		);
+
+		return $sections;
+	}
+}
