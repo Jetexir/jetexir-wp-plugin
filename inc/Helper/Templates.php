668af208c01f3e03f6371d5e52442b781b8741ba
Parsa Kafi <parselearn@gmail.com>
2025-03-23 14:46:56 +0330

    add: return type to Templates methods

--- inc/Helper/Templates.php
+++ inc/Helper/Templates.php
@@ -3,11 +3,11 @@
 namespace WooAssistant\Helper;
 
 class Templates {
-	public static function getPath( $file, $dir = 'woocommerce' ) {
+	public static function getPath( $file, $dir = 'woocommerce' ): string {
 		return self::pathCorrection( WOOASSISTANT_PLUGIN_PATH . '/inc/Templates/' . $dir . '/' . $file );
 	}
 
-	public static function pathCorrection( $path ) {
+	public static function pathCorrection( $path ): string {
 		return str_replace( [ '/', '\\' ], DIRECTORY_SEPARATOR, $path );
 	}
 }
