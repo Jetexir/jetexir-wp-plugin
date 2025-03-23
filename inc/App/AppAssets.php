b813397b5bd3167f8d02ed8990e1312608acf718
Parsa Kafi <parselearn@gmail.com>
2025-03-23 14:51:01 +0330

    add: Owl Carousel JS/CSS

--- inc/App/AppAssets.php
+++ inc/App/AppAssets.php
@@ -4,6 +4,7 @@ namespace WooAssistant\App;
 
 use WooAssistant\Helper\Assets;
 use WooAssistant\Helper\Nonce;
+use WooAssistant\Helper\Sanitizing;
 use WooAssistant\Helper\WordPress;
 
 defined( 'ABSPATH' ) || exit;
@@ -15,9 +16,10 @@ class AppAssets {
 
 	public function enqueueScripts(): void {
 		$pluginVersion = Assets::getVersion();
+		$debugName     = WOOASSISTANT_DEBUG_MODE ? '' : '.min';
 
 		wp_enqueue_style( WOOASSISTANT_PLUGIN_SLUG . '-global-style',
-			Assets::url( 'css/style.min.css' ), false, $pluginVersion );
+			Assets::url( 'css/style' . $debugName . '.css' ), false, $pluginVersion );
 
 		wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-global',
 			Assets::url( 'js/global.min.js' ),
@@ -29,7 +31,18 @@ class AppAssets {
 			'ajaxUrl'   => admin_url( 'admin-ajax.php' ),
 			'ajaxNonce' => Nonce::create(),
 			'sslError'  => __( 'Your site does not have SSL support, For example: https://example.com', 'woo-assistant' ),
-			'pageName'  => WordPress::getPageName()
+			'pageName'  => WordPress::getPageName(),
+			'direction' => WordPress::isRTL() ? 'rtl' : 'ltr',
 		) );
+
+		// Owl Carousel
+		wp_register_style( WOOASSISTANT_PLUGIN_SLUG . '-owl-carousel',
+			Assets::url( 'css/owl-carousel' . $debugName . '.css' ), false, '2.3.4' );
+		/*wp_register_style( WOOASSISTANT_PLUGIN_SLUG . '-owl-carousel-theme',
+			Assets::url( 'css/owl.theme.default' . $debugName . '.css' ), false, '2.3.4' );*/
+		wp_register_script( WOOASSISTANT_PLUGIN_SLUG . '-owl-carousel',
+			Assets::url( 'js/owl.carousel.min.js' ),
+			[ 'jquery' ], '2.3.4', [ 'in_footer' => true ] );
+
 	}
 }
