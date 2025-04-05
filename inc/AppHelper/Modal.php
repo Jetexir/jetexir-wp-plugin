eacdc74c691ba5dada2eeaffa7124e34b53b6fff
Parsa Kafi <parselearn@gmail.com>
2025-04-05 23:40:49 +0330

    add: admin modal hook

--- inc/AppHelper/Modal.php
+++ inc/AppHelper/Modal.php
@@ -5,15 +5,21 @@ namespace WooAssistant\AppHelper;
 class Modal {
 	public function __construct() {
 		add_action( 'wp_footer', [ $this, 'printModal' ] );
-		add_action( 'wp_footer', [ $this, 'printOverlay' ] );
+		add_action( 'admin_footer', [ $this, 'printAdminModal' ] );
+	}
+
+	public function printAdminModal(): void {
+		do_action( 'woo_assistant_admin_modals' );
+
+		if ( apply_filters( 'woo_assistant_admin_modal_overlay', true ) ) {
+			echo '<div id="wa-modal-overlay" class="wa-modal-overlay"></div>';
+		}
 	}
 
 	public function printModal(): void {
 		do_action( 'woo_assistant_site_modals' );
-	}
 
-	public function printOverlay(): void {
-		if ( apply_filters( 'woo_assistant_modal_overlay', false ) ) {
+		if ( apply_filters( 'woo_assistant_site_modal_overlay', false ) ) {
 			echo '<div id="wa-modal-overlay" class="wa-modal-overlay"></div>';
 		}
 	}
