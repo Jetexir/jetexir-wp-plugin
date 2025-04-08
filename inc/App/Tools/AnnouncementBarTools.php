7fa815f9bf06671407cbbba3fe53e128390d57bf
Parsa Kafi <parselearn@gmail.com>
2025-04-08 03:48:36 +0330

    add: display Announcement Bar in site

--- inc/App/Tools/AnnouncementBarTools.php
+++ inc/App/Tools/AnnouncementBarTools.php
@@ -3,9 +3,13 @@
 namespace WooAssistant\App\Tools;
 
 use WooAssistant\Addons\Addon;
+use WooAssistant\App\App;
+use WooAssistant\Helper\Assets;
+use WooAssistant\Helper\Helper;
 use WooAssistant\Helper\HTML;
 use WooAssistant\Helper\Notice;
 use WooAssistant\Helper\Templates;
+use WooAssistant\Helper\WordPress;
 use WooAssistant\Interfaces\AddonInterface;
 use WooAssistant\Providers\UI\DataTableUI;
 use WooAssistant\Settings\Settings;
@@ -13,11 +17,132 @@ use WooAssistant\Settings\Settings;
 class AnnouncementBarTools extends Addon implements AddonInterface {
 	public string $addonID = 'announcement-bar-tools';
 	private const sectionID = 'announcement-bar';
+	private const shortCode = 'wa_announcement_bar';
 
 	public function initAction(): void {
+		App::addShortcode( self::shortCode, [ $this, 'announcementBarShortcode' ] );
 		add_filter( 'woo_assistant_tools_settings_sections', [ $this, 'addSectionSettings' ] );
 		add_action( 'woo_assistant_data_table_ui_announcement_bar_action', [ $this, 'dataTableActions' ], 10, 2 );
 		add_filter( 'woo_assistant_tools_settings_display_footer', [ $this, 'displayFooterSettings' ], 10, 2 );
+	}
+
+	public function announcementBarShortcode( $atts ): string {
+		$atts = shortcode_atts( array(
+			'code' => '',
+		), $atts, self::shortCode );
+
+		if ( empty( $atts['code'] ) ) {
+			return '';
+		}
+
+		$announcement = $this->getAnnouncementByCode( $atts['code'] );
+		if ( ! $announcement ) {
+			return '';
+		}
+
+		return $this->getAnnouncement( $announcement, false );
+	}
+
+	public function wpBodyOpenAction(): void {
+		$announcements = $this->getAnnouncements( [ 'top', 'sticky-top' ] );
+
+		foreach ( $announcements as $announcement ) {
+			if ( $this->checkDisplay( $announcement ) ) {
+				echo $this->getAnnouncement( $announcement );
+			}
+		}
+	}
+
+	public function wpFooterAction(): void {
+		$announcements = $this->getAnnouncements( 'sticky-bottom' );
+
+		foreach ( $announcements as $announcement ) {
+			if ( $this->checkDisplay( $announcement ) ) {
+				echo $this->getAnnouncement( $announcement );
+			}
+		}
+	}
+
+	private function checkDisplay( $announcement ): bool {
+		$postIds   = $announcement['post_ids'] ?? '';
+		$displayOn = $announcement['display_on'] ?? [];
+
+		$display = false;
+
+		if ( in_array( 'all', $displayOn, true ) ) {
+			$display = true;
+		}
+
+		if ( ! $display && in_array( WordPress::getPageName(), $displayOn, true ) ) {
+			$display = true;
+		}
+
+		if ( ! $display && ! empty( $postIds ) && WordPress::isSingular() ) {
+			$postIds = explode( ',', $postIds );
+			$postIds = array_filter( $postIds );
+			$postIds = array_map( 'intval', $postIds );
+			$display = in_array( get_the_ID(), $postIds, true );
+		}
+
+		return $display;
+	}
+
+	public function getAnnouncement( $announcement, $position = true ): string {
+		$style = '--wa-announcement-bar-text-color: ' . ( $announcement['text_color'] ?? '#333' ) . ';';
+
+		$bgColorType = $announcement['bg_color_type'] ?? 'solid';
+		if ( $bgColorType === 'gradient' ) {
+			$bgColorGradient    = $announcement['bg_color_gradient'] ?? [];
+			$function           = $bgColorGradient['function'] = $bgColorGradient['function'] ?? 'linear-gradient';
+			$rotate             = $bgColorGradient['rotate'] = $bgColorGradient['rotate'] ?? 90;
+			$shape              = $bgColorGradient['shape'] = $bgColorGradient['shape'] ?? 'ellipse';
+			$colors             = isset( $bgColorGradient['colors'] ) && is_array( $bgColorGradient['colors'] ) && count( $bgColorGradient['colors'] ) >= 2 ? $bgColorGradient['colors'] : [];
+			$gradientFirstParam = $function === 'linear-gradient' ? $rotate . 'deg' : $shape;
+			$style              .= '--wa-announcement-bar-bg: ' . Assets::cssGradient( $colors, $function, $gradientFirstParam ) . ';';
+
+		} else {
+			$style .= '--wa-announcement-bar-bg: ' . $announcement['bg_color_solid'] ?? '#ebe5ff' . ';';
+		}
+
+		$tag         = 'div';
+		$withButtons = false;
+
+		if ( empty( $announcement['primary_button'] ) && ! empty( $announcement['primary_button_url'] ) ) {
+			$tag = 'a';
+		}
+		if ( ! empty( $announcement['primary_button'] ) || ! empty( $announcement['secondary_button'] ) ) {
+			$withButtons = true;
+		}
+
+		$output = '<' . $tag . ( $tag === 'a' ? ' href="' . $announcement['primary_button_url'] . '"' : '' ) . ' id="wa-announcement-bar-' . $announcement['code'] . '" class="wa-announcement-bar' . ( $position ? ' wa-announcement-bar-fixed wa-announcement-bar-' . $announcement['position'] : ' wa-announcement-bar-inline' ) . ( ! $withButtons ? ' wa-announcement-bar-center' : '' ) . '" style="' . $style . '">';
+		$output .= '<span class="wa-announcement-bar-container">';
+		$output .= '<span class="wa-announcement-bar-text">' . $announcement['text'] . '</span>';
+		if ( $withButtons ) {
+			$output .= '<span class="wa-announcement-bar-buttons">';
+			if ( ! empty( $announcement['primary_button'] ) && ! empty( $announcement['primary_button_url'] ) ) {
+				$output .= '<a href="' . $announcement['primary_button_url'] . '" class="wa-button wa-button-primary">' . $announcement['primary_button'] . '</a>';
+			}
+			if ( ! empty( $announcement['secondary_button'] ) && ! empty( $announcement['secondary_button_url'] ) ) {
+				$output .= '<a href="' . $announcement['secondary_button_url'] . '" class="wa-button wa-button-secondary">' . $announcement['secondary_button'] . '</a>';
+			}
+			$output .= '</span>';
+		}
+		$output .= '</span></' . $tag . '>';
+
+		return $output;
+	}
+
+	public function getAnnouncements( $position = null ): array {
+		$announcements = Settings::get( 'announcement_bar_data', [], self::sectionID );
+		$announcements = is_array( $announcements ) ? $announcements : [];
+
+		if ( ! empty( $announcements ) ) {
+			$announcements = array_filter( $announcements, static function ( $announcement ) use ( $position ) {
+				return $announcement['is_active'] && ( is_null( $position ) || $position === $announcement['position'] || ( is_array( $position ) && in_array( $announcement['position'], $position, true ) ) );
+			} );
+		}
+
+		return $announcements;
 	}
 
 	public function displayFooterSettings( $display, $section ): bool {
@@ -28,7 +153,20 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 		return $display;
 	}
 
-	private function getAnnouncement( $index ) {
+	private function getAnnouncementByCode( $code ) {
+		$announcements = Settings::get( 'announcement_bar_data', [], self::sectionID );
+		if ( is_array( $announcements ) && ! empty( $announcements ) ) {
+			foreach ( $announcements as $announcement ) {
+				if ( $announcement['code'] === $code ) {
+					return $announcement;
+				}
+			}
+		}
+
+		return false;
+	}
+
+	private function getAnnouncementByIndex( $index ) {
 		$announcements = Settings::get( 'announcement_bar_data', [], self::sectionID );
 		if ( is_array( $announcements ) && ! empty( $announcements ) && isset( $announcements[ $index ] ) ) {
 			return $announcements[ $index ];
@@ -48,7 +186,7 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 
 		} elseif ( $action === 'add_form' || $action === 'edit' ) {
 			$data = [];
-			if ( $index >= 0 && $announcement = $this->getAnnouncement( $index ) ) {
+			if ( $index >= 0 && $announcement = $this->getAnnouncementByIndex( $index ) ) {
 				$data = $announcement;
 			}
 
@@ -68,7 +206,7 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 			}
 
 			if ( $index >= 0 ) {
-				$announcement = $this->getAnnouncement( $index );
+				$announcement = $this->getAnnouncementByIndex( $index );
 
 				if ( $announcement === false ) {
 					$errorMessage = __( 'Announcement not found!', 'woo-assistant' );
@@ -88,12 +226,14 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 			}
 
 			if ( $announcement !== false ) {
-				$announcements           = Settings::get( 'announcement_bar_data', [], self::sectionID );
-				$announcements[ $index ] = $formData;
+				$announcements                   = Settings::get( 'announcement_bar_data', [], self::sectionID );
+				$announcements[ $index ]         = $formData;
+				$announcements[ $index ]['code'] = $announcement['code'];
 				Settings::save( 'announcement_bar_data', $announcements, self::sectionID );
 				$successMessage = __( 'The announcement was successfully saved.', 'woo-assistant' );
 
 			} else {
+				$formData['code'] = Helper::randomString( 6, true, false, true );
 				Settings::addToArray( 'announcement_bar_data', $formData, self::sectionID, true );
 				$successMessage = __( 'Announcement added successfully.', 'woo-assistant' );
 			}
@@ -139,8 +279,6 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 				], 403 );
 			}
 		}
-
-		//wp_send_json_success( [ $rowID => $rowAction ] );
 	}
 
 	private function getDataTable(): DataTableUI {
@@ -154,7 +292,10 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 		          ->addNewButton( __( 'Add new', 'woo-assistant' ) )
 		          ->addAction( 'edit', '<i class="wa-icon-edit"></i>', $dataTable::ACTION_EDIT )
 		          ->addAction( 'delete', '<i class="wa-icon-trash"></i>', $dataTable::ACTION_DELETE )
-		          ->addColumn( __( 'Title', 'woo-assistant' ), 'title', null, [ 'is_html' => true, ] )
+		          ->addColumn( __( 'Title', 'woo-assistant' ), 'title' )
+		          ->addColumn( __( 'ShortCode', 'woo-assistant' ), 'code', function ( $row ) {
+			          return '<code>[' . self::shortCode . ' code="' . $row['code'] . '"]</code>';
+		          }, [ 'is_html' => true ] )
 		          ->addColumn( __( 'Status', 'woo-assistant' ), $dataTable::ACTIVE_FIELD );
 
 		return $dataTable;
@@ -240,6 +381,21 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 				'type'  => 'startgrid',
 			),
 			array(
+				'id'                => 'position',
+				'title'             => __( 'Position', 'woo-assistant' ),
+				'type'              => 'select',
+				'options'           => array(
+					'top'           => __( 'Top', 'woo-assistant' ),
+					'sticky-top'    => __( 'Sticky on top', 'woo-assistant' ),
+					'sticky-bottom' => __( 'Sticky on bottom', 'woo-assistant' ),
+				),
+				'option_none'       => 'Use shortcode',
+				'option_none_value' => '',
+				'default'           => 'top',
+				'setting_value'     => $data['position'] ?? 'top',
+				'sanitize'          => 'text',
+			),
+			array(
 				'id'            => 'post_ids',
 				'title'         => __( 'Single post/page/product', 'woo-assistant' ),
 				'placeholder'   => '1,25,87',
@@ -291,7 +447,6 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 				'setting_value' => $data['text_color'] ?? '#333'
 			),
 			array(
-				'id'    => 'bg-color-type',
 				'title' => __( 'Background color type', 'woo-assistant' ),
 				'type'  => 'startInlineElements',
 			),
@@ -345,6 +500,15 @@ class AnnouncementBarTools extends Addon implements AddonInterface {
 				'type' => 'endgrid',
 			),
 		);
+	}
+
+	public function wpEnqueueScriptsAction(): void {
+		$pluginVersion = Assets::getVersion();
+		$debugName     = WOOASSISTANT_DEBUG_MODE ? '' : '.min';
+
+		wp_enqueue_style( WOOASSISTANT_PLUGIN_KEY . '-announcement-bar-style',
+			Assets::url( 'css/announcement-bar' . $debugName . '.css' ),
+			false, $pluginVersion );
 	}
 
 	public function info(): array {
