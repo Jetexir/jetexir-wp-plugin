fcac1ee9671c81aafa08a8a35dfcf6ef0a3561cf
Parsa Kafi <parselearn@gmail.com>
2025-04-20 01:01:55 +0330

    ref: Reorder new AdminSettings

--- inc/Admin/Admin.php
+++ inc/Admin/Admin.php
@@ -7,8 +7,8 @@ defined( 'ABSPATH' ) || exit;
 class Admin {
 	public function __construct() {
 		new AdminAssets();
-		new AdminPages();
 		new AdminSettings();
+		new AdminPages();
 
 		new AdminDashboard();
 		new AdminTools();
