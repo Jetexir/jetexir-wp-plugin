4f9f00bb703d429692cb47c01ae4acbf1db7c412
Parsa Kafi <parselearn@gmail.com>
2025-07-14 23:23:13 +0330

    add: Addon links to dashboard

--- inc/Admin/AdminDashboard.php
+++ inc/Admin/AdminDashboard.php
@@ -4,7 +4,9 @@ namespace WooAssistant\Admin;
 
 defined( 'ABSPATH' ) || exit;
 
+use WooAssistant\Addons\Addons;
 use WooAssistant\Helper\Notice;
+use WooAssistant\Helper\User;
 use WooAssistant\Interfaces\AdminTabInterface;
 
 class AdminDashboard implements AdminTabInterface {
@@ -16,14 +18,36 @@ class AdminDashboard implements AdminTabInterface {
 	}
 
 	public function notice(): void {
-
+		Notice::add( self::tab, __( 'Welcome to Woo Assistant!', 'woo-assistant' ), 'default' );
 	}
 
 	public function content(): void {
-		echo 'dashboard';
+		$addonList = $this->getAddons();
+		if ( empty( $addonList ) ) {
+			$message = '<strong>' . __( 'Hello', 'woo-assistant' ) . ', ' . User::getData( 'display_name' ) . '!</strong>';
+			$message .= '<p>' . __( 'Woo Assistant is here to help you sell more in your store. To get started, go to the Addons tab and activate the required addons.', 'woo-assistant' ) . '</p>';
+
+			echo '<div class="wa-dashboard-welcome">' . $message . '</div>';
+
+		} else {
+			echo '<div class="wa-dashboard-addons-wrap">';
+			foreach ( $addonList as $addon ) {
+				echo '<a href="' . $addon['link'] . '" title="' . $addon['desc'] . '">' . $addon['icon'] . '<span>' . $addon['title'] . '</span></a>';
+			}
+			echo '</div>';
+		}
 	}
 
-	public function settings(): array {
-		return [];
+	private function getAddons(): array {
+		$addons    = apply_filters( 'woo_assistant_dashboard_addons', [] );
+		$addonCats = Addons::getAddonCats();
+		$addonList = array();
+		foreach ( array_keys( $addonCats ) as $addonCat ) {
+			if ( ! empty( $addons[ $addonCat ] ) ) {
+				$addonList[] = $addons[ $addonCat ];
+			}
+		}
+
+		return array_merge( [], ...$addonList );
 	}
 }
