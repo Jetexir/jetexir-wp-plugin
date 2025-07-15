6d005a3e0510c3d84c906f160c117bab98806009
Parsa Kafi <parselearn@gmail.com>
2025-07-16 02:11:27 +0330

    add: custom links to dashboard

--- inc/Addons/Addons.php
+++ inc/Addons/Addons.php
@@ -17,6 +17,7 @@ defined( 'ABSPATH' ) || exit;
 
 class Addons {
 	public const tab = 'addons';
+	public const icon = '<svg fill="#873eff" viewBox="-1.6 -1.6 19.20 19.20" id="puzzle-16px" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"><path transform="translate(-1.6, -1.6), scale(0.6)" d="M16,26.067016359222563C19.01738168054809,25.872397068992516,22.262239430627645,26.517240315137514,24.60181605099669,24.60181605099669C27.111898440059782,22.546797774978174,28.145281087067747,19.24049670110675,28.296260704139346,16C28.455347682104016,12.585493874718981,27.663918047661323,9.155468547682407,25.442529248674575,6.557470751325425C23.042874131554072,3.750983787341677,19.68332835017655,1.3586644697408636,16,1.6190603700907609C12.450863972879969,1.8699694347691473,10.324639773941016,5.292883807362616,7.804411840049186,7.804411840049182C5.2767992788350275,10.323299008106623,1.883980163995068,12.445487433187566,1.5692449778710547,15.999999999999998C1.2381447568526107,19.73933372680073,3.1332333196882076,23.640132127311034,6.212055537857966,25.78794446214203C8.994928292718487,27.729299956421816,12.613919183695263,26.285416520322205,16,26.067016359222563" fill="#fff" strokewidth="0"></path></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path id="Path_56" data-name="Path 56" d="M-8.5,16h-4a.5.5,0,0,1-.5-.5v-1A1.5,1.5,0,0,0-14.5,13,1.5,1.5,0,0,0-16,14.5v1a.5.5,0,0,1-.5.5h-4a.5.5,0,0,1-.5-.5V3.5a.5.5,0,0,1,.5-.5H-17V2.5A2.5,2.5,0,0,1-14.5,0,2.5,2.5,0,0,1-12,2.5V3h3.5a.5.5,0,0,1,.5.5V7h.5A2.5,2.5,0,0,1-5,9.5,2.5,2.5,0,0,1-7.5,12H-8v3.5A.5.5,0,0,1-8.5,16ZM-12,15h3V11.5a.5.5,0,0,1,.5-.5h1A1.5,1.5,0,0,0-6,9.5,1.5,1.5,0,0,0-7.5,8h-1A.5.5,0,0,1-9,7.5V4h-3.5a.5.5,0,0,1-.5-.5v-1A1.5,1.5,0,0,0-14.5,1,1.5,1.5,0,0,0-16,2.5v1a.5.5,0,0,1-.5.5H-20V15h3v-.5A2.5,2.5,0,0,1-14.5,12,2.5,2.5,0,0,1-12,14.5Z" transform="translate(21)"></path> </g></svg>';
 
 	public function __construct() {
 		add_filter( 'woo_assistant_menus', [ $this, 'addMenu' ] );
@@ -26,8 +27,23 @@ class Addons {
 		add_filter( 'woo_assistant_' . self::tab . '_settings_display_reset_button', '__return_false' );
 		add_filter( 'woo_assistant_settings_submit_button_title', [ $this, 'changeSubmitButtonTitle' ], 10, 2 );
 		add_filter( 'woo_assistant_save_settings_success_message', [ $this, 'saveMessage' ], 10, 2 );
+		add_filter( 'woo_assistant_dashboard_custom_links', [ $this, 'addDashboardLink' ] );
 		add_action( 'woo_assistant_notice', [ $this, 'addRefreshNotice' ] );
 		add_action( 'admin_init', [ $this, 'flushRewriteRules' ] );
+	}
+
+	public function addDashboardLink( $links ) {
+		$links[] = [
+			'title' => __( 'Addons', 'woo-assistant' ),
+			'desc'  => __( 'Woo Assistant Addons', 'woo-assistant' ),
+			'link'  => AdminPages::link( [
+				'tab' => self::tab
+			] ),
+			'icon'  => self::icon,
+			'type'  => 'addons'
+		];
+
+		return $links;
 	}
 
 	public function flushRewriteRules(): void {
@@ -137,7 +153,7 @@ class Addons {
 			}
 
 			if ( empty( $icon ) && empty( $image ) ) {
-				$icon = '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="#873eff" stroke="#873eff" viewBox="0 0 36 36"><path d="M29.81 16H29V8.83a2 2 0 0 0-2-2h-6A5.14 5.14 0 0 0 16.51 2 5 5 0 0 0 11 6.83H4a2 2 0 0 0-2 2V17h2.81A3.13 3.13 0 0 1 8 19.69 3 3 0 0 1 7.22 22 3 3 0 0 1 5 23H2v8.83a2 2 0 0 0 2 2h23a2 2 0 0 0 2-2V26h1a5 5 0 0 0 5-5.51A5.15 5.15 0 0 0 29.81 16Zm2.41 7A3 3 0 0 1 30 24h-3v7.83H4V25h1a5 5 0 0 0 5-5.51A5.15 5.15 0 0 0 4.81 15H4V8.83h9V7a3 3 0 0 1 1-2.22A3 3 0 0 1 16.31 4 3.13 3.13 0 0 1 19 7.19v1.64h8V18h2.81A3.13 3.13 0 0 1 33 20.69a3 3 0 0 1-.78 2.31Z" class="clr-i-outline clr-i-outline-path-1"/></svg>';
+				$icon = self::icon;
 			}
 
 			$addonList[ $cat ][ $addon['id'] ] = array(
