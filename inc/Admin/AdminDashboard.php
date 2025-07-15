9e8e9f6f4a0924d7159cffed5689d55c5d4778d2
Parsa Kafi <parselearn@gmail.com>
2025-07-16 02:10:25 +0330

    feat: Add filter for add custom cart links to dashboard

--- inc/Admin/AdminDashboard.php
+++ inc/Admin/AdminDashboard.php
@@ -5,6 +5,7 @@ namespace WooAssistant\Admin;
 defined( 'ABSPATH' ) || exit;
 
 use WooAssistant\Addons\Addons;
+use WooAssistant\Helper\Assets;
 use WooAssistant\Helper\Notice;
 use WooAssistant\Helper\User;
 use WooAssistant\Interfaces\AdminTabInterface;
@@ -22,24 +23,30 @@ class AdminDashboard implements AdminTabInterface {
 	}
 
 	public function content(): void {
-		$addonList = $this->getAddons();
-		if ( empty( $addonList ) ) {
+		$dashboardTypeLinks = array(
+			'addons' => $this->getAddons(),
+			'custom' => apply_filters( 'woo_assistant_dashboard_custom_links', [] )
+		);
+
+		if ( empty( $dashboardTypeLinks['addons'] ) ) {
 			$message = '<strong>' . __( 'Hello', 'woo-assistant' ) . ', ' . User::getData( 'display_name' ) . '!</strong>';
 			$message .= '<p>' . __( 'Woo Assistant is here to help you sell more in your store. To get started, go to the Addons tab and activate the required addons.', 'woo-assistant' ) . '</p>';
 
 			echo '<div class="wa-dashboard-welcome">' . $message . '</div>';
-
-		} else {
-			echo '<div class="wa-dashboard-addons-wrap">';
-			foreach ( $addonList as $addon ) {
-				echo '<a href="' . $addon['link'] . '" title="' . $addon['desc'] . '">' . $addon['icon'] . '<span>' . $addon['title'] . '</span></a>';
-			}
-			echo '</div>';
 		}
+
+		echo '<div class="wa-dashboard-links-wrap">';
+		foreach ( $dashboardTypeLinks as $dashboardLinks ) {
+			foreach ( $dashboardLinks as $link ) {
+				$icon = ! empty( $link['icon'] ) && Assets::isSvgImageString( $link['icon'] ) ? Assets::setSvgDimensions( $link['icon'], 50 ) : '';
+				echo '<a href="' . $link['link'] . '" title="' . $link['desc'] . '" class="wa-link-type-' . $link['type'] . '">' . $icon . '<span>' . $link['title'] . '</span></a>';
+			}
+		}
+		echo '</div>';
 	}
 
 	private function getAddons(): array {
-		$addons    = apply_filters( 'woo_assistant_dashboard_addons', [] );
+		$addons    = apply_filters( 'woo_assistant_dashboard_addon_links', [] );
 		$addonCats = Addons::getAddonCats();
 		$addonList = array();
 		foreach ( array_keys( $addonCats ) as $addonCat ) {
