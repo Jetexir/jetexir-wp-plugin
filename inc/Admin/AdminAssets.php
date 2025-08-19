6f8cb97e6a0264ef3de28f6da13e06f7a0e4fb47
Parsa Kafi <parselearn@gmail.com>
2025-08-20 02:25:23 +0330

    refactor: condition for load assets

--- inc/Admin/AdminAssets.php
+++ inc/Admin/AdminAssets.php
@@ -13,44 +13,46 @@ class AdminAssets {
 	}
 
 	public function enqueueScripts(): void {
-		if ( AdminPages::isSettingPage() ) {
-			$pluginVersion = Assets::getVersion();
-			$debugName     = WOOASSISTANT_DEBUG_MODE ? '' : '.min';
-
-			wp_enqueue_media();
-			wp_enqueue_style( 'wp-color-picker' );
-			wp_enqueue_script( 'wp-color-picker' );
-
-			wp_enqueue_style( WOOASSISTANT_PLUGIN_SLUG . '-admin-style',
-				Assets::url( 'css-admin/admin-style' . $debugName . '.css' ), false, $pluginVersion );
-
-			wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-dom-drag',
-				Assets::url( 'js-admin/dom-drag.js' ),
-				[], $pluginVersion, [ 'in_footer' => true ] );
-
-			/*wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-modal',
-				Assets::url( 'js-admin/modal.min.js' ),
-				[], $pluginVersion, [ 'in_footer' => true ] );*/
-
-			wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-admin',
-				Assets::url( 'js-admin/script.min.js' ),
-				[
-					'jquery',
-					'jquery-ui-sortable',
-					WOOASSISTANT_PLUGIN_SLUG . '-dom-drag',
-					//WOOASSISTANT_PLUGIN_SLUG . '-modal'
-				], $pluginVersion, [ 'in_footer' => true ] );
-
-			wp_add_inline_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', 'var wooAssistantAjax = false, wooAssistantModalCloseEvent;', 'before' );
-
-			wp_localize_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', WOOASSISTANT_PLUGIN_KEYCAP, array(
-				'ajaxUrl'          => admin_url( 'admin-ajax.php' ),
-				'ajaxNonce'        => Nonce::create(),
-				'removeText'       => __( 'Remove', 'wc-assistant' ),
-				'dtuConfirmDelete' => __( 'Are you sure you want to delete this item(s)?', 'wc-assistant' ),
-				'copyText'        => __( 'Click to copy this text.', 'wc-assistant' ),
-			) );
+		if ( ! AdminPages::isSettingPage() ) {
+			return;
 		}
+
+		$pluginVersion = Assets::getVersion();
+		$debugName     = WOOASSISTANT_DEBUG_MODE ? '' : '.min';
+
+		wp_enqueue_media();
+		wp_enqueue_style( 'wp-color-picker' );
+		wp_enqueue_script( 'wp-color-picker' );
+
+		wp_enqueue_style( WOOASSISTANT_PLUGIN_SLUG . '-admin-style',
+			Assets::url( 'css-admin/admin-style' . $debugName . '.css' ), false, $pluginVersion );
+
+		wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-dom-drag',
+			Assets::url( 'js-admin/dom-drag.js' ),
+			[], $pluginVersion, [ 'in_footer' => true ] );
+
+		/*wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-modal',
+			Assets::url( 'js-admin/modal.min.js' ),
+			[], $pluginVersion, [ 'in_footer' => true ] );*/
+
+		wp_enqueue_script( WOOASSISTANT_PLUGIN_SLUG . '-admin',
+			Assets::url( 'js-admin/script.min.js' ),
+			[
+				'jquery',
+				'jquery-ui-sortable',
+				WOOASSISTANT_PLUGIN_SLUG . '-dom-drag',
+				//WOOASSISTANT_PLUGIN_SLUG . '-modal'
+			], $pluginVersion, [ 'in_footer' => true ] );
+
+		wp_add_inline_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', 'var wooAssistantAjax = false, wooAssistantModalCloseEvent;', 'before' );
+
+		wp_localize_script( WOOASSISTANT_PLUGIN_SLUG . '-admin', WOOASSISTANT_PLUGIN_KEYCAP, array(
+			'ajaxUrl'          => admin_url( 'admin-ajax.php' ),
+			'ajaxNonce'        => Nonce::create(),
+			'removeText'       => __( 'Remove', 'wc-assistant' ),
+			'dtuConfirmDelete' => __( 'Are you sure you want to delete this item(s)?', 'wc-assistant' ),
+			'copyText'         => __( 'Click to copy this text.', 'wc-assistant' ),
+		) );
 	}
 
 	public static function imageUrl( $path ): string {
