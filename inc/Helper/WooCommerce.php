9d73964325c80dce1cff76e8b0ceff7ffb102721
Parsa Kafi <parselearn@gmail.com>
2025-10-08 16:02:19 +0330

    chore: Simplified WooCommerce::hposEnabled method

--- inc/Helper/WooCommerce.php
+++ inc/Helper/WooCommerce.php
@@ -44,14 +44,9 @@ class WooCommerce {
 		return false;
 	}
 
-	public static function hposEnabled() {
-		if ( class_exists( '\Automattic\WooCommerce\Utilities\OrderUtil' ) ) {
-			return \Automattic\WooCommerce\Utilities\OrderUtil::custom_orders_table_usage_is_enabled();
-		}
-
-		return false;
+	public static function hposEnabled(): bool {
+		return class_exists( '\Automattic\WooCommerce\Utilities\OrderUtil' ) && \Automattic\WooCommerce\Utilities\OrderUtil::custom_orders_table_usage_is_enabled();
 	}
-
 
 	/**
 	 * Check page has block
